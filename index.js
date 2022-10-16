aa2dd7fa0ba5929a702691bec3633643450b1495
Дарья Иванова <dashann@mail.ru>
2022-10-17 00:44:33 +0300

    feat/add data trips in js and templates for html elements, change images format (png -> webp)

--- index.js
+++ index.js
@@ -1,132 +1,62 @@
-import {
-  departureToB,
-  departureToA,
-  priceTicketOneWay,
-  priceTicketRound,
-  timeOneWay,
-  timeBackElementShowSelector,
-  resultElementShowSelector
-} from './scripts/constants/constants.js';
+import {trips, departureSelector, descriptionSelector, overflowButtonText} from './scripts/constants/constants.js';
 
-const form = document.forms.calculator;
-const routeSelect = document.querySelector('#route');
-const timeSelect = document.querySelector('#time');
-const timeBackSelect = document.querySelector('#time-back');
-const numInput = document.querySelector('#num');
-const timeBackElement = document.querySelector('#label-of-time-back');
+const tripsSection = document.querySelector('.trips');
 
-const resultElement = document.querySelector('#result');
-const countElement = document.querySelector('#count');
-const tariffElement = document.querySelector('#tariff');
-const travelTimeElement = document.querySelector('#travel-time');
-const sumElement = document.querySelector('#sum');
-const departureElement = document.querySelector('#departure');
-const arrivalElement = document.querySelector('#arrival');
-
-const localeDepartureToB = getLocalTimeDepartures(departureToB);
-const localeDepartureToA = getLocalTimeDepartures(departureToA);
-
-// извлечение времени из исходных данных + его перевод в локальный часовой пояс
-function getLocalTimeDepartures(dateList) {
-  return dateList.map(item => {
-    const nowDate = new Date(item + ' GMT+3');
-    const nowHours = nowDate.getHours() > 9 ? nowDate.getHours() : '0' + nowDate.getHours();
-    const nowMinutes = nowDate.getMinutes() > 9 ? nowDate.getMinutes() : '0' + nowDate.getMinutes();
-    return nowHours + ':' + nowMinutes;
-  });
-}
-
-// отрисовка вариантов выбора из списка на странице
-function renderOptionList(valueList, selectSelector) {
-  selectSelector.innerHTML = '';
-  valueList.forEach(item => {
-    const option = document
-      .querySelector('.time-option__template')
+// отрисовка элементов страницы (элементы списков, карточек)
+function setLiElements(container, itemList, liClass) {
+  itemList.forEach(item => {
+    const liElement = document
+      .querySelector('.trip__li-template')
       .content
       .firstElementChild
       .cloneNode(true);
-    option.value = item;
-    option.textContent = item;
-    selectSelector.append(option);
+    liElement.classList.add(liClass);
+    liElement.textContent = item;
+    container.append(liElement);
   })
 }
 
-// счетчик времени прибытия
-function counterArrivalTime(departure, time) {
-  const hoursDeparture = +(departure.split(':')[0])
-  const minutesDeparture = +(departure.split(':')[1])
+function setDepartures(tripElement, departureList) {
+  const descriptionsBlock = tripElement.querySelector('.trip__descriptions');
 
-  const hoursArrival = minutesDeparture + time < 60
-    ? departure.split(':')[0]
-    : Math.floor((minutesDeparture + time) / 60) + hoursDeparture;
-  const minutesArrival = minutesDeparture + time < 60
-    ? minutesDeparture + time
-    : (minutesDeparture + time) % 60;
+  const descriptionWithDepartures = document
+    .querySelector('.trip__departures-template')
+    .content
+    .firstElementChild
+    .cloneNode(true);
 
-  return (hoursArrival > 9 ? hoursArrival : '0' + hoursArrival) + ':' + (minutesArrival > 9 ? minutesArrival : '0' + minutesArrival);
+  const shotDepartureList = departureList.length > 4 ? departureList.slice(0, 3).concat(overflowButtonText) : departureList;
+  const departuresContainer = descriptionWithDepartures.querySelector('.trip__departures');
+  setLiElements(departuresContainer, shotDepartureList, departureSelector);
+
+  descriptionsBlock.append(descriptionWithDepartures);
 }
 
-// отрисовка результатов подсчета на странице
-function setResults(config) {
-  const {tariff, departure, departureBack, number} = config;
-  const lastNumber = +number.toString().slice(-1)
-  const numberText = 'билет' + (lastNumber === 1 && (number < 2 || number > 20)
-    ? ''
-    : (lastNumber > 1 && lastNumber < 5) && (number < 5 || number > 20)
-      ? 'а'
-      : 'ов');
-  countElement.textContent = number + ' ' + numberText;
-  tariffElement.textContent = routeSelect[routeSelect.selectedIndex].textContent;
-  sumElement.textContent = (tariff === 'round' ? priceTicketRound : priceTicketOneWay) * number;
-  travelTimeElement.textContent = tariff === 'round' ? timeOneWay * 2 : timeOneWay;
-  departureElement.textContent = departure;
-  arrivalElement.textContent = counterArrivalTime(tariff === 'round' ? departureBack : departure, timeOneWay);
+function setDescriptions(tripElement, descriptionList, departureList) {
+  const descriptionsBlock = tripElement.querySelector('.trip__descriptions');
+  setLiElements(descriptionsBlock, descriptionList, descriptionSelector);
+  setDepartures(tripElement, departureList);
 }
 
+function renderTripList() {
+  trips.forEach(item => {
+    const trip = document
+      .querySelector('.trip-template')
+      .content
+      .firstElementChild
+      .cloneNode(true);
+    trip.querySelector('.trip__image').src = item.image;
+    item.label ? trip.querySelector('.trip__label').textContent = item.label : trip.querySelector('.trip__label').classList.add('trip__label_hidden');
+    trip.querySelector('.trip__time').textContent = item.time;
+    item.special && trip.querySelector('.trip__label').classList.add(`trip__label_content_${item.special}`);
+    trip.querySelector('.trip__sale').textContent = item.sale ? 'Акция - ' : '';
+    trip.querySelector('#caption').textContent = item.title;
+    trip.querySelector('#price').textContent = item.price;
+    trip.querySelector('#cash-price').textContent = item.priceCash;
+    setDescriptions(trip, item.options, item.departures);
 
-// обработчики событий формы
-function handleRouteChange(e) {
-  const value = e.target.value;
-
-  switch (value) {
-    case 'round':
-      renderOptionList(localeDepartureToB, timeSelect);
-      renderOptionList(localeDepartureToA.filter(item => item > counterArrivalTime(timeSelect.value, timeOneWay)), timeBackSelect);
-      timeBackElement.required = true;
-      timeBackElement.classList.add(timeBackElementShowSelector);
-      break;
-    case 'toA':
-      timeBackElement.classList.remove(timeBackElementShowSelector);
-      renderOptionList(localeDepartureToA, timeSelect);
-      break;
-    case 'toB':
-      timeBackElement.classList.remove(timeBackElementShowSelector);
-      renderOptionList(localeDepartureToB, timeSelect);
-      break;
-  }
+    tripsSection.append(trip);
+  })
 }
 
-function handleTimeChange(e) {
-  if (routeSelect.value === 'round') {
-    const newDepartureToA = localeDepartureToA.filter(item => item > counterArrivalTime(e.target.value, timeOneWay))
-    renderOptionList(newDepartureToA, timeBackSelect);
-  }
-}
-
-function handleSubmitForm(e) {
-  e.preventDefault();
-  resultElement.classList.add(resultElementShowSelector);
-
-  setResults({
-    tariff: routeSelect.value,
-    number: numInput.value,
-    departure: timeSelect.value,
-    departureBack: timeBackSelect.value
-  });
-}
-
-routeSelect.addEventListener('change', handleRouteChange);
-timeSelect.addEventListener('change', handleTimeChange);
-form.addEventListener('submit', handleSubmitForm);
-
-renderOptionList(localeDepartureToB, timeSelect);
+renderTripList();
---
+++ scripts/pages/order.js
@@ -0,0 +1,132 @@
+import {
+  departureToB,
+  departureToA,
+  priceTicketOneWay,
+  priceTicketRound,
+  timeOneWay,
+  timeBackElementShowSelector,
+  resultElementShowSelector,
+} from '../constants/constants.js';
+
+const form = document.forms.calculator;
+const routeSelect = document.querySelector('#route');
+const timeSelect = document.querySelector('#time');
+const timeBackSelect = document.querySelector('#time-back');
+const numInput = document.querySelector('#num');
+const timeBackElement = document.querySelector('#label-of-time-back');
+
+const resultElement = document.querySelector('#result');
+const countElement = document.querySelector('#count');
+const tariffElement = document.querySelector('#tariff');
+const travelTimeElement = document.querySelector('#travel-time');
+const sumElement = document.querySelector('#sum');
+const departureElement = document.querySelector('#departure');
+const arrivalElement = document.querySelector('#arrival');
+
+const localeDepartureToB = getLocalTimeDepartures(departureToB);
+const localeDepartureToA = getLocalTimeDepartures(departureToA);
+
+// извлечение времени из исходных данных + его перевод в локальный часовой пояс
+function getLocalTimeDepartures(dateList) {
+  return dateList.map(item => {
+    const nowDate = new Date(item + ' GMT+3');
+    const nowHours = nowDate.getHours() > 9 ? nowDate.getHours() : '0' + nowDate.getHours();
+    const nowMinutes = nowDate.getMinutes() > 9 ? nowDate.getMinutes() : '0' + nowDate.getMinutes();
+    return nowHours + ':' + nowMinutes;
+  });
+}
+
+// отрисовка вариантов выбора из списка на странице
+function renderOptionList(valueList, select) {
+  select.innerHTML = '';
+  valueList.forEach(item => {
+    const option = document
+      .querySelector(`.time-option__template`)
+      .content
+      .firstElementChild
+      .cloneNode(true);
+    option.value = item;
+    option.textContent = item;
+    select.append(option);
+  })
+}
+
+// счетчик времени прибытия
+function counterArrivalTime(departure, time) {
+  const hoursDeparture = +(departure.split(':')[0])
+  const minutesDeparture = +(departure.split(':')[1])
+
+  const hoursArrival = minutesDeparture + time < 60
+    ? departure.split(':')[0]
+    : Math.floor((minutesDeparture + time) / 60) + hoursDeparture;
+  const minutesArrival = minutesDeparture + time < 60
+    ? minutesDeparture + time
+    : (minutesDeparture + time) % 60;
+
+  return (hoursArrival > 9 ? hoursArrival : '0' + hoursArrival) + ':' + (minutesArrival > 9 ? minutesArrival : '0' + minutesArrival);
+}
+
+// отрисовка результатов подсчета на странице
+function setResults(config) {
+  const {tariff, departure, departureBack, number} = config;
+  const lastNumber = +number.toString().slice(-1)
+  const numberText = 'билет' + (lastNumber === 1 && (number < 2 || number > 20)
+    ? ''
+    : (lastNumber > 1 && lastNumber < 5) && (number < 5 || number > 20)
+      ? 'а'
+      : 'ов');
+  countElement.textContent = number + ' ' + numberText;
+  tariffElement.textContent = routeSelect[routeSelect.selectedIndex].textContent;
+  sumElement.textContent = (tariff === 'round' ? priceTicketRound : priceTicketOneWay) * number;
+  travelTimeElement.textContent = tariff === 'round' ? timeOneWay * 2 : timeOneWay;
+  departureElement.textContent = departure;
+  arrivalElement.textContent = counterArrivalTime(tariff === 'round' ? departureBack : departure, timeOneWay);
+}
+
+
+// обработчики событий формы
+function handleRouteChange(e) {
+  const value = e.target.value;
+
+  switch (value) {
+    case 'round':
+      renderOptionList(localeDepartureToB, timeSelect);
+      renderOptionList(localeDepartureToA.filter(item => item > counterArrivalTime(timeSelect.value, timeOneWay)), timeBackSelect);
+      timeBackElement.required = true;
+      timeBackElement.classList.add(timeBackElementShowSelector);
+      break;
+    case 'toA':
+      timeBackElement.classList.remove(timeBackElementShowSelector);
+      renderOptionList(localeDepartureToA, timeSelect);
+      break;
+    case 'toB':
+      timeBackElement.classList.remove(timeBackElementShowSelector);
+      renderOptionList(localeDepartureToB, timeSelect);
+      break;
+  }
+}
+
+function handleTimeChange(e) {
+  if (routeSelect.value === 'round') {
+    const newDepartureToA = localeDepartureToA.filter(item => item > counterArrivalTime(e.target.value, timeOneWay))
+    renderOptionList(newDepartureToA, timeBackSelect);
+  }
+}
+
+function handleSubmitForm(e) {
+  e.preventDefault();
+  resultElement.classList.add(resultElementShowSelector);
+
+  setResults({
+    tariff: routeSelect.value,
+    number: numInput.value,
+    departure: timeSelect.value,
+    departureBack: timeBackSelect.value
+  });
+}
+
+routeSelect.addEventListener('change', handleRouteChange);
+timeSelect.addEventListener('change', handleTimeChange);
+form.addEventListener('submit', handleSubmitForm);
+
+renderOptionList(localeDepartureToB, timeSelect);
